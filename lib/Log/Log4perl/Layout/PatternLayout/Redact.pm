1d837f84aaf96baeb466417b232c2c4c3e7d4da1
Kate Kirby <kate@thinkgeek.com>
2012-09-28 10:24:44 -0400

    Build for 1.2.1

--- lib/Log/Log4perl/Layout/PatternLayout/Redact.pm
+++ lib/Log/Log4perl/Layout/PatternLayout/Redact.pm
@@ -37,11 +37,11 @@ can be PCI-compliant.
 
 =head1 VERSION
 
-Version 1.2.0
+Version 1.2.1
 
 =cut
 
-our $VERSION = '1.2.0';
+our $VERSION = '1.2.1';
 
 our $SENSITIVE_ARGUMENT_NAMES = undef;
 our $SENSITIVE_REGEXP_PATTERNS = undef;
@@ -249,10 +249,11 @@ and write code for them!
 
 Copyright 2012 Kate Kirby & Guillaume Aubert.
 
-This program is free software; you can redistribute it and/or modify it
-under the terms of the Artistic License.
+This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License version 3 as published by the Free Software Foundation.
 
-See http://dev.perl.org/licenses/ for more information.
+This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with this program. If not, see http://www.gnu.org/licenses/
 
 =cut
 
